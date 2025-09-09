a75640b3bab5be1a3917ce5df2fc1c7c743c6750
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-09 16:41:15 +0530

    optimized code and refactored variable names

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const rockPaperScissors = ["rock", "paper", "scissors"];
 const colors = ["#0074B6", "#BD00FF", "#FFA943"];
 const images = ["scissor.png", "paper.png", "rock.png"];
 const rpsTriangle = document.querySelector(".rps-triangle");
-const decider = document.querySelector(".decider");
-const heading1 = document.querySelector(".heading1");
-const heading2 = document.querySelector(".heading2");
+const deciderDiv = document.querySelector(".decider");
+const title = document.querySelector(".heading1");
+const semiTitle = document.querySelector(".heading2");
 const rulesBtn = document.querySelector(".rules-btn");
 const rulesPopup = document.querySelector(".rules-popup");
 const nextBtn = document.querySelector(".next-btn");
@@ -23,22 +23,10 @@ const computerScore = document.querySelector(".computer-score");
 const yourScore = document.querySelector(".your-score");
 
 const scores = [0, 0];
+function togglePopup(flag = true) {
+  rulesPopup.style.display = flag ? "block" : "none";
+}
 
-function showPopup() {
-  const rulesPopup = document.getElementsByClassName("rules-popup")[0];
-  console.log(rulesPopup.style);
-  const display = rulesPopup.style.display;
-  if (display === "" || display === "none") {
-    rulesPopup.style.display = "block";
-  }
-}
-function hidePopup() {
-  const rulesPopup = document.getElementsByClassName("rules-popup")[0];
-  const display = rulesPopup.style.display;
-  if (display === "block") {
-    rulesPopup.style.display = "none";
-  }
-}
 function selectionHandler(key) {
   if (userValue !== null) {
     return;
@@ -47,14 +35,13 @@ function selectionHandler(key) {
   computerValue = Math.floor(Math.random() * 3);
   if (userValue === computerValue) {
     result = 2;
-    heading1.textContent = "TIE UP";
-    heading2.textContent = "";
+    title.textContent = "TIE UP";
+    semiTitle.textContent = "";
+    
     player1Winner.style.display = "none";
     player2Winner.style.display = "none";
     player1Normal.style.display = "block";
     player2Normal.style.display = "block";
-    /*border: 1rem solid #0074b6;
-            background-image: url('scissor.png'); */
     player1Normal.style.border = `1rem solid ${colors[userValue]}`;
     player1Normal.style.backgroundImage = `url(${images[userValue]})`;
     player2Normal.style.border = `1rem solid ${colors[computerValue]}`;
@@ -74,15 +61,16 @@ function selectionHandler(key) {
       player2Normal.style.backgroundImage = `url(${images[computerValue]})`;
       player1Winner1.style.border = `1rem solid ${colors[userValue]}`;
       player1Winner1.style.backgroundImage = `url(${images[userValue]})`;
-      heading1.textContent = "YOU WIN";
+      title.textContent = "YOU WIN";
       scores[0]++;
       localStorage.setItem("scores-rps", scores.toString());
-      rulesBtn.style.right = "13%";
+      rulesBtn.style.right = "10%";
       nextBtn.style.display = "block";
-      rulesPopup.style.right = "13%";
+      rulesPopup.style.right = "10%";
     } else {
-      heading1.textContent = "YOU LOST";
+      title.textContent = "YOU LOST";
       result = 1;
+      scores[1]++;
       player1Winner.style.display = "none";
       player2Winner.style.display = "flex";
       player1Normal.style.display = "block";
@@ -91,24 +79,23 @@ function selectionHandler(key) {
       player1Normal.style.backgroundImage = `url(${images[userValue]})`;
       player2Winner1.style.border = `1rem solid ${colors[computerValue]}`;
       player2Winner1.style.backgroundImage = `url(${images[computerValue]})`;
-      scores[1]++;
+      
       localStorage.setItem("scores-rps", scores.toString());
     }
-    heading2.textContent = "AGAINST PC";
+    semiTitle.textContent = "AGAINST PC";
   }
   yourScore.textContent = scores[0];
   computerScore.textContent = scores[1];
   rpsTriangle.style.display = "none";
-  decider.style.display = "flex";
+  deciderDiv.style.display = "flex";
 }
 function reset() {
   rpsTriangle.style.display = "block";
-  decider.style.display = "none";
+  deciderDiv.style.display = "none";
   rulesBtn.style.right = "2%";
   nextBtn.style.display = "none";
-    hurray.style.display="none";
-    scoreDiv.style.display = "flex";
-
+  hurray.style.display = "none";
+  scoreDiv.style.display = "flex";
   rulesPopup.style.right = "2%";
   userValue = null;
   computerValue = null;
@@ -116,25 +103,19 @@ function reset() {
 }
 function next() {
   scoreDiv.style.display = "none";
-  decider.style.display = "none";
+  deciderDiv.style.display = "none";
   rulesBtn.style.right = "2%";
   nextBtn.style.display = "none";
   rulesPopup.style.right = "2%";
-  hurray.style.display="flex";
-}
-function playAgain(){
-
+  hurray.style.display = "flex";
 }
 document.addEventListener("DOMContentLoaded", function () {
-  console.log(localStorage.getItem("scores-rps"));
   let localScores = localStorage.getItem("scores-rps");
   if (localScores) {
     let splitArray = localScores.split(",");
-    console.log(splitArray);
     scores[0] = splitArray[0];
     scores[1] = splitArray[1];
   }
-  console.log(scores);
   yourScore.textContent = scores[0];
   computerScore.textContent = scores[1];
 });
