6f48b6e6072c531fa5e27fcd68189f980fe78c86
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-09 16:09:04 +0530

    Added hurray functionality

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ let userValue = null,
   computerValue = null,
   result = null;
 const rockPaperScissors = ["rock", "paper", "scissors"];
-const colors = ["#0074B6","#BD00FF","#FFA943"];
-const images = ["scissor.png","paper.png","rock.png"]
+const colors = ["#0074B6", "#BD00FF", "#FFA943"];
+const images = ["scissor.png", "paper.png", "rock.png"];
 const rpsTriangle = document.querySelector(".rps-triangle");
 const decider = document.querySelector(".decider");
 const heading1 = document.querySelector(".heading1");
 const heading2 = document.querySelector(".heading2");
-const rulesBtn =document.querySelector(".rules-btn");
+const rulesBtn = document.querySelector(".rules-btn");
 const rulesPopup = document.querySelector(".rules-popup");
-const nextBtn =document.querySelector(".next-btn");
+const nextBtn = document.querySelector(".next-btn");
+const scoreDiv = document.querySelector(".score");
+const hurray = document.querySelector(".hurray-container");
 const player1Normal = document.querySelector(".player1-normal");
 const player1Winner = document.querySelector(".player1-winner");
 const player1Winner1 = document.querySelector(".player1-winner-1");
 const player2Normal = document.querySelector(".player2-normal");
 const player2Winner = document.querySelector(".player2-winner");
 const player2Winner1 = document.querySelector(".player2-winner-1");
-const computerScore =document.querySelector(".computer-score");
-const yourScore =document.querySelector(".your-score");
+const computerScore = document.querySelector(".computer-score");
+const yourScore = document.querySelector(".your-score");
 
-const scores = [0,0]
+const scores = [0, 0];
 
 function showPopup() {
   const rulesPopup = document.getElementsByClassName("rules-popup")[0];
@@ -53,10 +55,10 @@ function selectionHandler(key) {
     player2Normal.style.display = "block";
     /*border: 1rem solid #0074b6;
             background-image: url('scissor.png'); */
-    player1Normal.style.border = `1rem solid ${colors[userValue]}`
-    player1Normal.style.backgroundImage = `url(${images[userValue]})`
-    player2Normal.style.border = `1rem solid ${colors[computerValue]}`
-    player2Normal.style.backgroundImage = `url(${images[computerValue]})`
+    player1Normal.style.border = `1rem solid ${colors[userValue]}`;
+    player1Normal.style.backgroundImage = `url(${images[userValue]})`;
+    player2Normal.style.border = `1rem solid ${colors[computerValue]}`;
+    player2Normal.style.backgroundImage = `url(${images[computerValue]})`;
   } else {
     if (
       (userValue === 0 && computerValue === 2) ||
@@ -68,16 +70,16 @@ function selectionHandler(key) {
       player2Winner.style.display = "none";
       player1Normal.style.display = "none";
       player2Normal.style.display = "block";
-      player2Normal.style.border = `1rem solid ${colors[computerValue]}`
-      player2Normal.style.backgroundImage = `url(${images[computerValue]})`
-      player1Winner1.style.border = `1rem solid ${colors[userValue]}`
-      player1Winner1.style.backgroundImage = `url(${images[userValue]})`
+      player2Normal.style.border = `1rem solid ${colors[computerValue]}`;
+      player2Normal.style.backgroundImage = `url(${images[computerValue]})`;
+      player1Winner1.style.border = `1rem solid ${colors[userValue]}`;
+      player1Winner1.style.backgroundImage = `url(${images[userValue]})`;
       heading1.textContent = "YOU WIN";
       scores[0]++;
-      localStorage.setItem("scores-rps",scores.toString());
-      rulesBtn.style.right='13%';
-      nextBtn.style.display='block';
-      rulesPopup.style.right='13%';
+      localStorage.setItem("scores-rps", scores.toString());
+      rulesBtn.style.right = "13%";
+      nextBtn.style.display = "block";
+      rulesPopup.style.right = "13%";
     } else {
       heading1.textContent = "YOU LOST";
       result = 1;
@@ -85,42 +87,54 @@ function selectionHandler(key) {
       player2Winner.style.display = "flex";
       player1Normal.style.display = "block";
       player2Normal.style.display = "none";
-       player1Normal.style.border = `1rem solid ${colors[userValue]}`
-      player1Normal.style.backgroundImage = `url(${images[userValue]})`
-      player2Winner1.style.border = `1rem solid ${colors[computerValue]}`
+      player1Normal.style.border = `1rem solid ${colors[userValue]}`;
+      player1Normal.style.backgroundImage = `url(${images[userValue]})`;
+      player2Winner1.style.border = `1rem solid ${colors[computerValue]}`;
       player2Winner1.style.backgroundImage = `url(${images[computerValue]})`;
       scores[1]++;
-     localStorage.setItem("scores-rps",scores.toString())
-
+      localStorage.setItem("scores-rps", scores.toString());
     }
     heading2.textContent = "AGAINST PC";
   }
-  yourScore.textContent=scores[0];
-  computerScore.textContent=scores[1];
+  yourScore.textContent = scores[0];
+  computerScore.textContent = scores[1];
   rpsTriangle.style.display = "none";
   decider.style.display = "flex";
 }
-function reset(){
-    rpsTriangle.style.display = "block";
-    decider.style.display = "none";
-    rulesBtn.style.right='2%';
-      nextBtn.style.display='none';
-      rulesPopup.style.right='2%';
-    userValue = null;
+function reset() {
+  rpsTriangle.style.display = "block";
+  decider.style.display = "none";
+  rulesBtn.style.right = "2%";
+  nextBtn.style.display = "none";
+    hurray.style.display="none";
+    scoreDiv.style.display = "flex";
+
+  rulesPopup.style.right = "2%";
+  userValue = null;
   computerValue = null;
   result = null;
 }
-document.addEventListener("DOMContentLoaded",function(){
-    console.log(localStorage.getItem("scores-rps"));
-    let localScores = localStorage.getItem("scores-rps");
-    if(localScores){
-        let splitArray = localScores.split(",");
-        console.log(splitArray)
-        scores[0]=(splitArray[0]);
-        scores[1]=(splitArray[1]);
-    }
-    console.log(scores)
-    yourScore.textContent=scores[0];
-  computerScore.textContent=scores[1];
+function next() {
+  scoreDiv.style.display = "none";
+  decider.style.display = "none";
+  rulesBtn.style.right = "2%";
+  nextBtn.style.display = "none";
+  rulesPopup.style.right = "2%";
+  hurray.style.display="flex";
+}
+function playAgain(){
 
-})
+}
+document.addEventListener("DOMContentLoaded", function () {
+  console.log(localStorage.getItem("scores-rps"));
+  let localScores = localStorage.getItem("scores-rps");
+  if (localScores) {
+    let splitArray = localScores.split(",");
+    console.log(splitArray);
+    scores[0] = splitArray[0];
+    scores[1] = splitArray[1];
+  }
+  console.log(scores);
+  yourScore.textContent = scores[0];
+  computerScore.textContent = scores[1];
+});
