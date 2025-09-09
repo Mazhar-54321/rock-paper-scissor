509d2efca5820bc5bc0bf81ef1712b468089349e
Mazhar-54321 <syedmazharali742@gmail.com>
2025-09-09 13:44:51 +0530

    Added functionality of rock paper scissors

---
+++ index.js
@@ -0,0 +1,93 @@
+let userValue = null,
+  computerValue = null,
+  result = null;
+const rockPaperScissors = ["rock", "paper", "scissors"];
+const colors = ["#0074B6","#BD00FF","#FFA943"];
+const images = ["scissor.png","paper.png","rock.png"]
+const rpsTriangle = document.querySelector(".rps-triangle");
+const decider = document.querySelector(".decider");
+const heading1 = document.querySelector(".heading1");
+const heading2 = document.querySelector(".heading2");
+const player1Normal = document.querySelector(".player1-normal");
+const player1Winner = document.querySelector(".player1-winner");
+const player1Winner1 = document.querySelector(".player1-winner-1");
+const player2Normal = document.querySelector(".player2-normal");
+const player2Winner = document.querySelector(".player2-winner");
+const player2Winner1 = document.querySelector(".player2-winner-1");
+
+
+function showPopup() {
+  const rulesPopup = document.getElementsByClassName("rules-popup")[0];
+  console.log(rulesPopup.style);
+  const display = rulesPopup.style.display;
+  if (display === "" || display === "none") {
+    rulesPopup.style.display = "block";
+  }
+}
+function hidePopup() {
+  const rulesPopup = document.getElementsByClassName("rules-popup")[0];
+  const display = rulesPopup.style.display;
+  if (display === "block") {
+    rulesPopup.style.display = "none";
+  }
+}
+function selectionHandler(key) {
+  if (userValue !== null) {
+    return;
+  }
+  userValue = key;
+  computerValue = Math.floor(Math.random() * 3);
+  if (userValue === computerValue) {
+    result = 2;
+    heading1.textContent = "TIE UP";
+    heading2.textContent = "";
+    player1Winner.style.display = "none";
+    player2Winner.style.display = "none";
+    player1Normal.style.display = "block";
+    player2Normal.style.display = "block";
+    /*border: 1rem solid #0074b6;
+            background-image: url('scissor.png'); */
+    player1Normal.style.border = `1rem solid ${colors[userValue]}`
+    player1Normal.style.backgroundImage = `url(${images[userValue]})`
+    player2Normal.style.border = `1rem solid ${colors[computerValue]}`
+    player2Normal.style.backgroundImage = `url(${images[computerValue]})`
+  } else {
+    if (
+      (userValue === 0 && computerValue === 2) ||
+      (userValue === 1 && computerValue === 0) ||
+      (userValue === 2 && computerValue === 1)
+    ) {
+      result = 0;
+      player1Winner.style.display = "flex";
+      player2Winner.style.display = "none";
+      player1Normal.style.display = "none";
+      player2Normal.style.display = "block";
+      player2Normal.style.border = `1rem solid ${colors[computerValue]}`
+      player2Normal.style.backgroundImage = `url(${images[computerValue]})`
+      player1Winner1.style.border = `1rem solid ${colors[userValue]}`
+      player1Winner1.style.backgroundImage = `url(${images[userValue]})`
+      heading1.textContent = "YOU WIN";
+    } else {
+      heading1.textContent = "YOU LOST";
+      result = 1;
+      player1Winner.style.display = "none";
+      player2Winner.style.display = "flex";
+      player1Normal.style.display = "block";
+      player2Normal.style.display = "none";
+       player1Normal.style.border = `1rem solid ${colors[userValue]}`
+      player1Normal.style.backgroundImage = `url(${images[userValue]})`
+      player2Winner1.style.border = `1rem solid ${colors[computerValue]}`
+      player2Winner1.style.backgroundImage = `url(${images[computerValue]})`
+    }
+    heading2.textContent = "AGAINST PC";
+  }
+  rpsTriangle.style.display = "none";
+  decider.style.display = "flex";
+}
+function reset(){
+    rpsTriangle.style.display = "block";
+    decider.style.display = "none";
+    userValue = null;
+  computerValue = null;
+  result = null;
+}
